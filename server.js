5396c6fbc490b4fdbdf0715d92e225c8f4a9adae
rmanojqa <rmanoj.qa@gmail.com>
2023-04-17 21:21:38 +0530

    fix

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const { chromium } = require("playwright");
 
 (async () => {
-  const browser = await chromium.launchServer();
+  try {
+    const browser = await chromium.launchServer();
 
-  console.log(browser.wsEndpoint());
+    console.log(browser.wsEndpoint());
+  } catch (error) {
+    console.log(error);
+  }
 })();
